af15a9f211a6eb714acdd0ad1bbba56d07f02f5e
Eva Antipina <eva.antipina@gmail.com>
2018-04-20 12:49:44 -0700

    Add cshtml files and add HTTP methods and routs to ContactController. It will display home page with the list of contacts, link to the page with form to create new contact, and Clear My Contacts button, which delete all contacts from the address book. The user can click on any contact name and it will redirect them to Details page, where this contact details will be displayed. The user will be able to delete every contact individualy by hiting Delete Contact button. This action will redirect user the Delete page where will be a message that the contact has been deleted.

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -13,5 +13,44 @@ namespace AddressBook.Controllers
             List<Contact> allContacts = Contact.GetAll();
             return View(allContacts);
         }
+
+        [HttpGet("/contacts/new")]
+        public ActionResult Form()
+        {
+            Console.WriteLine("I am in the Form");
+            return View();
+        }
+
+        [HttpPost("/contacts/new")]
+        public ActionResult Create()
+        {
+            Contact newContact = new Contact (Request.Form["name"],Request.Form["phoneNumber"],Request.Form["address"]);
+            newContact.Save();
+            List<Contact> allContacts = Contact.GetAll();
+            return View("Index", allContacts);
+        }
+
+        [HttpGet("/details/{id}")]
+        public ActionResult Details(int id)
+        {
+            Contact contact = Contact.Find(id);
+            return View(contact);
+        }
+
+        [HttpPost("/contacts/delete/{id}")]
+        public ActionResult DeleteContact(int id)
+        {
+             Contact.ClearContact(id);
+             List<Contact> allContacts = Contact.GetAll();
+             return View("Delete", allContacts);
+        }
+
+        [HttpPost("/contacts/delete")]
+        public ActionResult Delete()
+        {
+             Contact.ClearAll();
+             List<Contact> allContacts = Contact.GetAll();
+             return View(allContacts);
+        }
     }
 }
--- Models/Contacts.cs
+++ Models/Contacts.cs
@@ -10,6 +10,8 @@ namespace AddressBook.Models
         private string _name;
         private string _phoneNumber;
         private string _address;
+        private int _id;
+        private static int _counter = 0;
         private static  List<Contact> _contacts = new List<Contact> {};
 
         public Contact (string name, string phoneNumber, string address)
@@ -17,31 +19,42 @@ namespace AddressBook.Models
             _name = name;
             _phoneNumber = phoneNumber;
             _address = address;
+            _id = _counter++;
         }
 
         public string GetName()
         {
            return _name;
         }
+
         public void SetName(string name)
         {
            _name = name;
         }
+
         public string GetPhoneNumber()
         {
            return _phoneNumber;
         }
+
         public void SetPhoneNumber(string phoneNumber)
         {
            _phoneNumber = phoneNumber;
         }
+
         public string GetAddress()
         {
            return _address;
         }
+
         public void SetAddress(string address)
         {
            _address = address;
+        }
+
+        public int GetId()
+        {
+            return _id;
         }
 
         public void Save()
@@ -53,9 +66,27 @@ namespace AddressBook.Models
         {
             return _contacts;
         }
-        public static List<Contact> ClearAll()
+        public static void ClearAll()
         {
             _contacts.Clear();
+        }
+
+        public static Contact Find(int searchId)
+        {
+            return _contacts.Single(c => c._id == searchId);
+        }
+        public static void ClearContact(int id)
+        {
+            int i;
+            for (i = 0; i < _contacts.Count; i++)
+            {
+                if (_contacts[i]._id == id) {
+                    break;
+                }
+             }
+             if (i != _contacts.Count) {
+                _contacts.RemoveAt(i);
+             }
         }
     }
 }
